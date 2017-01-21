2daf3c3ba9c6c540c4107ae0a849e9298673c532
Isha Goyal <iamishagoyal@gmail.com>
2017-01-22 02:34:43 +0530

    search in js working

--- WikiViewer.js
+++ WikiViewer.js
@@ -1,55 +1,29 @@
-$(document).ready(function(){
-  $('input').keyup(function(evt){
-     if(evt.which===13){
-       var userInput=$('input').val();
-       getResults(userInput);
-     }
+$(document).ready(function() {
+  $('input').keyup(function(evt) {
+    if (evt.which == 13) {
+      var userInput = $('input').val();
+      console.log(userInput);
+      getResults(userInput);
+    }
   });
 });
 
-function getResults(userInput){
-  var url = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + userInput + '&callback=?';
+function getResults(userInput) {
+  var url='https://en.wikipedia.org/w/api.php?'
+    + 'action=query&list=search&format=json&srprop=snippet'
+    + '&srsearch=' + userInput + '&callback=?';
   
- /*$.ajax({
-   url:url,
-   dataType:'jsonp',
-   success: function(responseData){
-     var entries='';
-     
-     for(var i=0;i<responseData.query.search.length;i++){
-       entries+=
-         createEntry(responseData.query.search[i]);
-      
-}
- $('.wrapper').html(entries);
-     }
-   
- });*/
-  $.getJSON(url, function(data){
-    
-    var entries=data.query.search;
-    entries.forEach(function (entry){
-      
-      var urlTitle=entry.title.split(' ');
-      urlTitle=urlTitle.join('_');
-      ('$.wrapper').append("<div><h3><a target='_blank' href='https://en.wikipedia.org/wiki/" + urlTitle + ">" + entry.title + "</a></h3> <p>" + entry.snippet + "</p> </div>");
-    });
-    
+  $.ajax({
+    url:url,
+    dataType:'jsonp',
+    success: function (data){
+      var results=data.query.search;
+      console.log(results);
+      $.each(results,function(key,value){
+         $('.wrapper').append("<div><h3><a target='_blank' href=''https://en.wikipedia.org/wiki/" + value.title +  "'>" + value.title + "</a></h3><p>" + value.snippet + "</p></div>");
+      });
+    }
   });
+     
 }
-            
-     /*       function createEntry(entry) {
-  var wikiBase = 'http://wikipedia.org/wiki/';
-  var titleLink = '<a href="'
-                  + wikiBase
-                  + entry.title
-                  + '">'
-                  + entry.title
-                  + '</a>';
-  var html = '<div class="entry"><div><span class="title">'
-              + titleLink
-              + '</span><br><br><span'
-              + entry.snippet
-              + '</span></div></div>';
-  return html;
-}*/
+
