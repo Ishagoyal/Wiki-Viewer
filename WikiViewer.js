4949c98d1f3dcbe43a8fe2997300aa2309740ce6
Isha Goyal <iamishagoyal@gmail.com>
2017-05-01 23:56:22 +0530

    revised version working

--- WikiViewer.js
+++ WikiViewer.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $('input').keyup(function(evt) {
     if (evt.which == 13) {
       var userInput = $('input').val();
-      console.log(userInput);
       getResults(userInput);
     }
   });
@@ -18,7 +17,6 @@ function getResults(userInput) {
     dataType:'jsonp',
     success: function (data){
       var results=data.query.search;
-      console.log(results);
       $.each(results,function(key,value){
          $('.wrapper').append("<div><h3><a id='heading' target='_blank' href='https://en.wikipedia.org/wiki/" + value.title +  "'>" + value.title + "</a></h3><p id='content'>" + value.snippet + "</p></div>");
       });
